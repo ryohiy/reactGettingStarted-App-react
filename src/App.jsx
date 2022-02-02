2e2329b01a31332cb0f5edeb658ba52516c2ab78
tanakenDX <90129428+tanakenDX@users.noreply.github.com>
2022-02-02 22:11:43 +0900

    0202_jsx導入

--- src/App.jsx
+++ src/App.jsx
@@ -3,5 +3,32 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="todoを入力" />
+        <button>追加</button>
+      </div>
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <div>
+            <li>ああああ</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <div>
+            <li>うううう</li>
+            <button>完了</button>
+            <button>削除</button>
+          </div>
+        </ul>
+      </div>
+    </>
+  );
 };
